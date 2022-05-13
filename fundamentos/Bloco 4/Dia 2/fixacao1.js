db43bfdd521f9fddddbc171793843aff77eac9c2
Guilherme Vargas <gvargas159z@gmail.com>
2022-05-13 15:33:43 -0300

    fixaçao

---
+++ fundamentos/Bloco 4/Dia 2/fixacao1.js
@@ -0,0 +1,6 @@
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+  }
+  // Laço de Repetição para uma lista de compras.
